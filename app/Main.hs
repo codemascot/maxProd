9a4fb71babf300e8382f2d9865bd00e364316712
Khan Mohammad R <codemascot@hotmail.com>
2023-02-18 15:32:41 +0100

    Quicksort and Merge Sort separated to Lib

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,8 @@
 module Main where
 
 import Text.Read
-
--- Quicksort Starts
-qsort :: (Ord a) => [a] -> [a]
-qsort []     = []
-qsort (x:xs) = lowest ++ [x] ++ highest
-  where lowest  = qsort [a | a <- xs, a <= x]
-        highest = qsort [a | a <- xs, a > x]
--- Quicksort Ends
-
--- Merge Sort Starts
--- Divide a list into two halves
-divide :: (Ord a) => [a] -> ([a], [a])
-divide []     = ([], [])
-divide (x:xs) = (x:ys, zs)
-  where
-    (zs, ys) = divide xs
-
--- Merge two sorted lists
-merge :: (Ord a) => [a] -> [a] -> [a]
-merge [] []         = []
-merge [] ys         = ys
-merge xs []         = xs
-merge (x:xs) (y:ys) | x <= y    = x:(merge xs (y:ys))
-                    | otherwise = y:(merge (x:xs) ys)
-
--- Sort the list using the merge sort algorithm
-msort :: (Ord a) => [a] -> [a]
-msort []  = []
-msort [x] = [x]
-msort xs  = (merge . msort) x $ msort y
-  where
-    (x, y) = divide xs
--- Merge Sort Ends
+import Lib.Quicksort as Q
+import Lib.MergeSort as M
 
 lastN :: Int -> [a] -> [a]
 lastN n xs = drop (length xs - n) xs
@@ -66,7 +35,7 @@ main = do
         else do
         let { sortedList :: [Int]
             -- ; sortedList = qsort ints
-            ; sortedList = msort ints
+            ; sortedList = M.msort ints
             ; prodOfFirst2AndLast :: Int
             ; prodOfFirst2AndLast = getProduct $ getFirstTwoAndLast sortedList
             ; prodOfLast3 :: Int
